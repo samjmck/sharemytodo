077964479b8edd7ce0f140858835d2891b89a55e
Sam Mckenzie <25828757+samjmck@users.noreply.github.com>
2023-12-22 20:54:25 +0100

    Fix duplicate script tag

--- site/src/main.ts
+++ site/src/main.ts
@@ -7,11 +7,4 @@ const app = new App({
   target: document.getElementById('app')
 });
 
-if(window.location.pathname === "/") {
-  document.body.innerHTML += `
-  <script async defer src="https://a.sharemytodo.com/latest.js"></script>
-  <noscript><img src="https://a.sharemytodo.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
-  `;
-}
-
 export default app;
